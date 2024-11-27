797b6453aabdaf008e1f826fbd856c28ca63decf
martin.bonetti <1martinbonetti@gmail.com>
2024-11-27 09:07:50 +0100

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Classroom;
+use App\Models\Transport;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'is_admin' => 1,
+        ]);
+
+        Classroom::factory(5)->create();
+        Transport::factory(5)->create();
+    }
+}
